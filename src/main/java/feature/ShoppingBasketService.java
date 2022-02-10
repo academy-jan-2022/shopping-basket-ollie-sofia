11bd0192560af892abafa57b22cff6f51922b550
oawood93 <oa.wood@outlook.com>
2022-02-10 15:36:55 +0000

    reset basketFor method

--- src/main/java/feature/ShoppingBasketService.java
+++ src/main/java/feature/ShoppingBasketService.java
@@ -10,18 +10,9 @@ public class ShoppingBasketService {
 
     public void addItem(UserId userId, ProductId productId, int quantity) {
         basketRepo.addUserItem(userId, productId, quantity);
-//        var newItem = new Item(product, quantity);
-//        var userBasket = basketRepo.getByUserId(userId);
-//
-//        if (userBasket == null) {
-//            userBasket = basketRepo.createBasket(userId);
-//        }
-//
-//        basketRepo.addItem(userBasket, newItem);
     }
 
     public Basket basketFor(UserId userId) {
-
-        return basketRepo.getByUserId(userId);
+        throw new UnsupportedOperationException();
     }
 }
