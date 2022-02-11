12fc0f467a0ce781ba40983ff5ab2ae8a44c834e
sofia <sofia.carballo@codurance.com>
2022-02-11 10:52:18 +0100

    red

--- src/test/java/feature/ProductRepositoryShould.java
+++ src/test/java/feature/ProductRepositoryShould.java
@@ -1,6 +1,18 @@
 package feature;
 
+import feature.Domain.ProductId;
+import feature.Repositories.ProductRepositoryInMem;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ProductRepositoryShould {
 
-    //@Test
+    @Test
+    void
+    return_expected_product() {
+        var productRepo = new ProductRepositoryInMem();
+        var result = productRepo.get(new ProductId(1));
+        assertEquals(1, result);
+    }
 }
